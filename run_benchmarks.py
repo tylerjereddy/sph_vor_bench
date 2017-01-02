52fa59f57b9c0886bf9d24d68ffbb44a62d20ca1
Tyler Reddy <tyler.je.reddy@gmail.com>
2017-01-01 17:45:20 -0700

    Initial draft of run_benchmarks.py, the code that should execute the benchmarks.

---
+++ run_benchmarks.py
@@ -0,0 +1,19 @@
+'''Run the SphericalVoronoi benchmarks.'''
+
+import benchlib
+import scipy
+
+version = scipy.__version__
+
+if '0.18.1' in version:
+    benchlib.benchmark_SphericalVoronoi(max_num_generators=140000,
+                                        num_tests=5,
+                                        num_repeats=5,
+                                        outfile_name='bench_018.p')
+elif '0.19' in version:
+    benchlib.benchmark_SphericalVoronoi(max_num_generators=1e6,
+                                        num_tests=5,
+                                        num_repeats=5,
+                                        outfile_name='bench_019.p')
+else:
+    print 'Invalid scipy version for benchmarks:', version
