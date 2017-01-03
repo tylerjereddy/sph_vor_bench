fdb74c2120a4e54275adfd630e1c046b11618672
Tyler Reddy <tyler.je.reddy@gmail.com>
2017-01-03 15:04:57 -0700

    A vertical line has been added to the 0.18.1 vs. 0.19 scipy SphericalVoronoi benchmark plot, where a 16 GB machine runs out of memory using scipy 0.18.1.

--- plot_benchmarks.py
+++ plot_benchmarks.py
@@ -32,4 +32,7 @@ ax1.set_xscale('log')
 ax1.set_xlim(1,10**8)
 ax1.annotate('dengue', (4000, 5), (400, 50), arrowprops =dict(arrowstyle="->"),fontsize = 10) #approx. dengue leaflet size
 ax1.annotate('influenza A', (20000, 12), (1000, 70), arrowprops = dict(arrowstyle="->"),fontsize = 10) #approx. flu leaflet size indicator
+# add vertical line at scipy 0.18.1 physical memory consumption limit
+# on rMBP 
+ax1.axvline(x=9e4, lw=4, color='grey', ls='--')
 fig.savefig('fig_benchmarks.png', dpi=300)
