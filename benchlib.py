f8cb283100db4d5dd5fb169491eaa1b581af94ba
Tyler Reddy <tyler.je.reddy@gmail.com>
2017-01-03 18:02:13 -0700

    Drafted scipy 0.19 time complexity fitting code in benchlib.py

--- benchlib.py
+++ benchlib.py
@@ -5,6 +5,7 @@ import time
 import scipy
 from scipy.spatial import SphericalVoronoi
 import cPickle as pickle
+import scipy.optimize
 
 def generate_spherical_points(num_points):
     # generate uniform points on sphere (see:
@@ -34,3 +35,48 @@ def benchmark_SphericalVoronoi(max_num_generators, num_tests, num_repeats,
         dict_performance_measures[generator_count] = {'avg':avg_execution_time,
                                                       'std':std_execution_time}
     pickle.dump(dict_performance_measures, open(outfile_name, 'wb'))
+
+def fit_time_complexity(scipy_019_bench_data):
+
+    def loglinear(n, K):
+        '''Loglinear fit with K as the constant.'''
+        return K * (n * np.log(n))
+
+    def linear(n, K):
+        '''Linear fit with K as the constant.'''
+        return K * n
+
+    def quadratic(n, K):
+        '''Quadratic fit with K as the constant.'''
+        return K * (n ** 2)
+
+    x_data = []
+    y_data = []
+    for generator_count, subdict in scipy_019_bench_data.iteritems():
+        x_data.append(generator_count)
+        y_data.append(subdict['avg'])
+    x_data = np.array(x_data)
+    y_data = np.array(y_data)
+    sorter = np.argsort(x_data)
+    x_data = x_data[sorter]
+    y_data = y_data[sorter]
+
+    K_loglinear, pcov_loglinear = sp.optimize.curve_fit(loglinear,
+            x_data, y_data)
+    K_linear, pcov_linear = sp.optimize.curve_fit(linear,
+            x_data, y_data)
+    K_quadratic, pcov_quadratic = sp.optimize.curve_fit(quadratic,
+            x_data, y_data)
+
+    sample_x_data = np.linspace(1, x_data.max(), num=50)
+    sample_y_data_loglinear = loglinear(sample_x_data, K_loglinear[0])
+    sample_y_data_linear = linear(sample_x_data, K_linear[0])
+    sample_y_data_quadratic = quadratic(sample_x_data, K_quadratic[0])
+
+    return_dict = {'sample_x_data':sample_x_data,
+                   'loglinear':sample_y_data_loglinear, 
+                   'linear':sample_y_data_linear,
+                   'quadratic':sample_y_data_quadratic}
+    return return_dict
+
+
